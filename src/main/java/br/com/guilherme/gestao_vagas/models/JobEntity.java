772f07ed7974686d06802740d5038aad1c7c2402
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2025-03-02 23:19:19 -0300

    Criado entity job e relacionamento com company

---
+++ src/main/java/br/com/guilherme/gestao_vagas/models/JobEntity.java
@@ -0,0 +1,34 @@
+package br.com.guilherme.gestao_vagas.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private String level;
+
+    private String benefits;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyEntity;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
