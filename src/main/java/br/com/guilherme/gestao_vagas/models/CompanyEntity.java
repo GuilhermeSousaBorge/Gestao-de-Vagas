a5311c47590d0ef2bcbf5c8865e246c2f55b6ef3
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2025-03-02 23:09:04 -0300

    Criado entity company

---
+++ src/main/java/br/com/guilherme/gestao_vagas/models/CompanyEntity.java
@@ -0,0 +1,33 @@
+package br.com.guilherme.gestao_vagas.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    @Email(message = "Insira um email valido!")
+    private String email;
+    @Length(min = 6, max = 8, message = "O campo [senha] deve conter 6 a 8 caracteres")
+    private String password;
+    @Pattern(regexp = "\\S+", message = "[username] invalido")
+    private String userName;
+    private String webSite;
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
